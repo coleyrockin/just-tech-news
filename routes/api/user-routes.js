63d79c8b2a14eab50db83eb6ca67e6f93dbe61fe
coleyrockin <coleyrockin@aol.com>
2022-08-16 17:29:08 -0500

    routes,passowrd,login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,6 +47,32 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+      // add comment syntax in front of this line in the .then()
+      //res.json({ user: dbUserData });
+  
+      // Verify user
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+  
+    });  
+  });
+
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
@@ -62,7 +88,7 @@ router.put('/:id', (req, res) => {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
-      res.json(dbUserData);
+     res.json(dbUserData);
     })
     .catch(err => {
       console.log(err);
